6f2def0e8f800c2f77eef63b620214ff19e3db6e
AnjaliNayak <83298803+AnjaliNayak@users.noreply.github.com>
2022-07-11 14:50:07 +0530

    Update code.py

--- madlibs_sample/code.py
+++ madlibs_sample/code.py
@@ -1,1 +1,8 @@
-
+def madlib():
+  adj1 = input("Adjective: ")
+  body_part = input("Body part: ")
+  
+  madlib = f"I love computer programming because it's {adj1}! The journey to becoming a \
+good programmer starts with hope in your mind and a dream in your {body_part}."
+  
+  print(madlib)
