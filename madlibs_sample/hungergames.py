7f61152c7fa05141b99d7bc07247eb5981295c05
AnjaliNayak <83298803+AnjaliNayak@users.noreply.github.com>
2022-07-13 23:47:10 +0530

    Update hungergames.py

--- madlibs_sample/hungergames.py
+++ madlibs_sample/hungergames.py
@@ -1,1 +1,8 @@
-
+def madlib():
+  number = input("Number: ")
+  verb = input("Verb: ")
+  noun = input("Noun: ")
+  
+  madlib=f"{number} seconds. That’s how long we’re required to {verb} on our metal circles before \
+the sound of a {noun} releases us."
+  print(madlib)
